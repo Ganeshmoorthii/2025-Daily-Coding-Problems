7b0908d91dc5368694ee1368f7d3d04b5c712b9b
Ganesha Moorthi. M <152759878+Ganeshmoorthii@users.noreply.github.com>
2025-01-02 14:13:45 +0530

    Create Subarrays with sum K.java

---
+++ DAY 2/Subarrays with sum K.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countSubarrays(int[] arr, int k) {
+        int n = arr.length;
+        Map<Integer, Integer> mpp = new HashMap<>();
+        int preSum = 0, cnt = 0;
+
+        mpp.put(0, 1); 
+
+        for (int i = 0; i < n; i++) {
+            preSum += arr[i];
+            int remove = preSum - k;
+
+            cnt += mpp.getOrDefault(remove, 0);
+            
+            mpp.put(preSum, mpp.getOrDefault(preSum, 0) + 1);
+        }
+        return cnt;
+    }
+}
